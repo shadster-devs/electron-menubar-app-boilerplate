e68469efa954247b6b1ef5c32ba97709d5e361e4
shakthi.m <shakthi.m@go-mmt.com>
2025-07-15 16:21:58 +0530

    auto update test v1.3.9

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,73 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-  target: 'electron-renderer',
-  entry: './src/renderer/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist/renderer'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-              '@babel/preset-typescript'
-            ]
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    target: 'electron-renderer',
+    entry: './src/renderer/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist/renderer'),
+      filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
+      clean: true, // Clean output directory before each build
+    },
+    // Remove source maps in production, use faster ones in development
+    devtool: isProduction ? false : 'eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', { targets: { electron: '25' } }],
+                '@babel/preset-react',
+                '@babel/preset-typescript'
+              ]
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/renderer/index.html'
-    })
-  ]
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/renderer/index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        } : false
+      })
+    ],
+    optimization: {
+      minimize: isProduction,
+      splitChunks: isProduction ? {
+        chunks: 'all',
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      } : false,
+    },
+  };
 }; 
