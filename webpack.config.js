aa2bebf8bfd12531123daed80574dbee6364d35e
shakthi.m <shakthi.m@go-mmt.com>
2025-07-21 18:51:58 +0530

    please work bro v1.4.15

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -11,6 +12,7 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'dist/renderer'),
       filename: isProduction ? 'bundle.[contenthash].js' : 'bundle.js',
       clean: true, // Clean output directory before each build
+      publicPath: './', // Important for Electron file:// protocol
     },
     // Remove source maps in production, use faster ones in development
     devtool: isProduction ? false : 'eval-source-map',
@@ -37,11 +39,44 @@ module.exports = (env, argv) => {
       ]
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx']
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      fallback: {
+        // Provide fallbacks for Node.js modules that might be referenced
+        "path": false,
+        "fs": false,
+        "crypto": false,
+        "stream": false,
+        "util": false,
+        "buffer": false,
+        "assert": false,
+        "http": false,
+        "https": false,
+        "os": false,
+        "url": false,
+        "querystring": false,
+        "timers": false,
+        "console": false,
+        "vm": false,
+        "zlib": false,
+        "tty": false,
+        "domain": false,
+        "events": false,
+        "punycode": false,
+        "string_decoder": false,
+        "child_process": false,
+        "cluster": false,
+        "dgram": false,
+        "dns": false,
+        "net": false,
+        "readline": false,
+        "repl": false,
+        "tls": false,
+      }
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/renderer/index.html',
+        inject: 'body', // Automatically inject script tags
         minify: isProduction ? {
           removeComments: true,
           collapseWhitespace: true,
@@ -54,7 +89,12 @@ module.exports = (env, argv) => {
           minifyCSS: true,
           minifyURLs: true,
         } : false
-      })
+      }),
+      // Define global variables for renderer process
+      new webpack.DefinePlugin({
+        'global': 'globalThis', // Fix global undefined error
+        'process.env.NODE_ENV': JSON.stringify(isProduction ? 'production' : 'development'),
+      }),
     ],
     optimization: {
       minimize: isProduction,
